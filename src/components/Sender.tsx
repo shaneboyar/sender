681b8fb8dc866dc6afe2b8aae71bbcc15e96d313
Shane Boyar <shane.boyar@gmail.com>
2020-12-14 19:38:48 -0500

    add test message post

--- src/components/Sender.tsx
+++ src/components/Sender.tsx
@@ -6,9 +6,22 @@ export interface SenderProps {
 
 const Sender = ({ name }: SenderProps) => {
   const [message, setMessage] = useState("");
+
+  const sendNotification = () => {
+    fetch("http://localhost:3001", {
+      method: "POST",
+      body: JSON.stringify({
+        message: "test",
+      }),
+    });
+  };
+
   return (
     <div className="flex flex-col p-8 h-64 w-48 shadow rounded">
       <div className="text-center text-lg font-bold">{name}</div>
+      <div className="border rounded" onClick={sendNotification}>
+        Send Message
+      </div>
     </div>
   );
 };
